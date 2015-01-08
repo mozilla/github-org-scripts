aaf3223818cf9c0b68b91e0c340982d19219b285
Fred Wenzel <fwenzel@mozilla.com>
2015-01-08 14:43:40 -0800

    Identify repos with or without CONTRIBUTING file

--- README.md
+++ README.md
@@ -4,5 +4,16 @@ This is a small helper script to analyze all the "sources" (i.e., original repos
 
 This ensures that all repositories in an org make explicit what the requirements for contributing code to the organization are, be it a simple "this is how you submit a successful pull request" or "you have to sign this agreement before you can contribute".
 
+## Caveats
+For now this is somewhat hardcoded for the mozilla org; no need for it to remain that way though.
+
+## Auth
+The API calls need you to authenticate. Generate a "personal access token" on the github settings page, then create a file ``.credentials`` like this:
+
+    johndoe
+    123abcmygithubtoken123...
+
+where the first line is your github username and the second line is the token you generated.
+
 ## License
 This code is free software and licensed under an MIT license. &copy; 2015 Fred Wenzel <fwenzel@mozilla.com>. For more information read the file ``LICENSE``.
---
+++ contributing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import sys
+
+from github3 import login
+
+
+CREDENTIALS_FILE = '.credentials'
+
+
+if __name__ == '__main__':
+    id = token = ''
+    with open(CREDENTIALS_FILE, 'r') as cf:
+        id = cf.readline().strip()
+        token = cf.readline().strip()
+
+    gh = login(token=token)
+
+    # Divvy up into repositories that need/do not need a CONTRIBUTING file.
+    good_repos = []
+    bad_repos = []
+
+    repos = gh.organization('mozilla').iter_repos(type='sources')
+    for repo in repos:
+        # All files in this repo's default branch.
+        # {'filename.md': Content(), 'filename2.txt': Contents(), ...}
+        files = repo.contents('/')
+
+        if files:
+            contrib_files = [f for f in files.keys() if
+                             f[0].startswith('CONTRIBUTING')]
+        else:
+            contrib_files = None
+
+        if contrib_files:
+            good_repos.append(repo.name)
+        else:
+            bad_repos.append(repo.name)
+
+        sys.stderr.write('.')
+
+    good_repos.sort()
+    print
+    print 'The following repos HAVE a CONTRIBUTING file:'
+    for r in good_repos:
+        print r
+
+    bad_repos.sort()
+    print
+    print
+    print 'The following repos DO NOT HAVE a CONTRIBUTING file:'
+    for r in bad_repos:
+        print r
