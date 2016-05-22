d6450d37bc1da3af7c4e59d33b0d960dc77112f2
Hal Wine <hwine@mozilla.com>
2016-05-22 13:37:08 -0700

    Upgrade to github3.py direct usage.
    Previously, we only read the first page (response) of admins without 2fa
    enabled.  Now that we're also looking at all members, the results may
    spread to multiple responses.
    
    Github3.py module has support for just returning iterators, which avoids
    all the hassles of page handling and response parsing and 'magic header'
    request creation. \o/

--- admins.py
+++ admins.py
@@ -1,53 +1,42 @@
 #!/usr/bin/env python
-import urllib
+"""
+    Check for admins or any member without 2FA enabled.
+
+    List contact information if available for members out of compliance.
+"""
 import argparse
 import logging
 
-import requests
-
-from client import get_token
+from client import get_github3_client
 
 logger = logging.getLogger(__name__)
 
-def get_user_email(user_login, headers):
-    user_url = 'https://api.github.com/users/%s' % user_login
-    resp = requests.get(user_url, headers=headers)
-    user_info = resp.json()
-    email = user_info.get('email', '') or "no public email address"
+def get_user_email(gh, user_login):
+    user_info = gh.user(user_login)
+    email = user_info.email or "no public email address"
     return email
 
-def check_admins(org):
-    headers = {
-        # New API requires new accept per
-        # https://developer.github.com/changes/2015-06-24-api-enhancements-for-working-with-organization-permissions/#preview-period
-        'Accept': 'application/vnd.github.ironman-preview+json',
-        'Authorization': 'token %s' % get_token()
-    }
-    params = {'filter': '2fa_disabled',
-              'role': 'admin'}
+def check_users(gh, org_name, admins_only=True):
 
-    admins_with_1fa_url = '%s?%s' % (
-        'https://api.github.com/orgs/%s/members' % org,
-        urllib.urlencode(params)
-    )
-    resp = requests.get(admins_with_1fa_url, headers=headers)
-    if resp.status_code not in (200,):
-        logging.warn("Failed request for %s (code %d)", org,
-                     resp.status_code)
-        return
+    org = gh.organization(org_name)
 
-    real_admins = resp.json()
+    role = 'admin' if admins_only else 'all'
+    user_type = 'admins' if admins_only else 'members'
+    bad_member = list(org.members(filter='2fa_disabled', role=role))
 
-    if real_admins:
-        print 'The following admins DO NOT HAVE 2FA for org %s:' % org
-        for a in real_admins:
-            print a['login'], get_user_email(a['login'], headers)
+    if bad_member:
+        print('The following %d %s DO NOT HAVE 2FA for org %s:' %
+                (len(bad_member), user_type, org_name))
+        for a in bad_member:
+            print a.login, get_user_email(gh, a.login)
     else:
-        print 'Congrats! All admins have 2FA enabled in %s!' % org
-
+        print('Congrats! All %s have 2FA enabled in %s!' % (user_type,
+            org_name))
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--admins', action='store_true',
+                        help='Report only for admins (default all members)')
     parser.add_argument("orgs", nargs='*', default=['mozilla', ],
                         help='github organizations to check (defaults to mozilla)')
     return parser.parse_args()
@@ -55,8 +44,10 @@ def parse_args():
 
 def main():
     args = parse_args()
-    for org in args.orgs:
-        check_admins(org)
+    if args.orgs:
+        gh = get_github3_client()
+        for org in args.orgs:
+            check_users(gh, org, args.admins)
 
 
 if __name__ == '__main__':
