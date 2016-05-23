266e5e516a7e8812786d7f87242d4856b05fb025
Hal Wine <hwine@mozilla.com>
2016-05-23 14:30:36 -0700

    Keep a specified team updated with 2FA status
    Since we don't always have email addresses for accounts, to communicate
    with accounts we set up a special team, and update membership based on
    current query results.
    
    NOTE: unlike other uses of the missing_2fa.py script, this one requires
    an access token with write:org permissions to update the team.

--- missing_2fa.py
+++ missing_2fa.py
@@ -9,6 +9,7 @@ import logging
 
 from client import get_github3_client
 
+BAD_TEAM = '2fa_disabled'
 logger = logging.getLogger(__name__)
 
 def get_user_email(gh, user_login):
@@ -16,7 +17,34 @@ def get_user_email(gh, user_login):
     email = user_info.email or "no public email address"
     return email
 
-def check_users(gh, org_name, admins_only=True):
+def update_team_membership(org, new_member_list):
+    # assume we're using a team to communicate with these folks, update
+    # that team to contain exactly new_member_list members
+    # team & repo assignment must be done already
+    team = [x for x in org.teams() if x.name == BAD_TEAM][0]
+    # get set of current members
+    current = set([x.login for x in team.members()])
+    # get set of new members
+    new = set([x.login for x in new_member_list])
+    to_remove = current - new
+    to_add = new - current
+    no_change = new & current
+    print "%5d alumni" % len(to_remove)
+    for login in to_remove:
+        if not team.remove_member(login):
+            logger.warn("Failed to remove a member"
+                    " - you need 'write:org' permissions")
+            break
+    print "%5d new" % len(to_add)
+    for login in to_add:
+        if not team.add_member(login):
+            logger.warn("Failed to add a member"
+                    " - you need 'write:org' permissions")
+            break
+    print "%5d no change" % len(no_change)
+
+
+def check_users(gh, org_name, admins_only=True, update_team=False):
 
     org = gh.organization(org_name)
 
@@ -25,13 +53,19 @@ def check_users(gh, org_name, admins_only=True):
     bad_member = list(org.members(filter='2fa_disabled', role=role))
 
     if bad_member:
-        print('The following %d %s DO NOT HAVE 2FA for org %s:' %
-                (len(bad_member), user_type, org_name))
-        for a in bad_member:
-            print a.login, get_user_email(gh, a.login)
+        if update_team:
+            print('There are %d %s that DO NOT HAVE 2FA for org %s:' %
+                    (len(bad_member), user_type, org_name))
+        else:
+            print('The following %d %s DO NOT HAVE 2FA for org %s:' %
+                    (len(bad_member), user_type, org_name))
+            for a in bad_member:
+                print a.login, get_user_email(gh, a.login)
     else:
         print('Congrats! All %s have 2FA enabled in %s!' % (user_type,
             org_name))
+    if update_team:
+        update_team_membership(org, bad_member)
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -39,6 +73,8 @@ def parse_args():
                         help='Report only for admins (default all members)')
     parser.add_argument("orgs", nargs='*', default=['mozilla', ],
                         help='github organizations to check (defaults to mozilla)')
+    parser.add_argument("--update-team", action='store_true', 
+                        help='update membership of team "%s"' % BAD_TEAM)
     return parser.parse_args()
 
 
@@ -47,7 +83,7 @@ def main():
     if args.orgs:
         gh = get_github3_client()
         for org in args.orgs:
-            check_users(gh, org, args.admins)
+            check_users(gh, org, args.admins, args.update_team)
 
 
 if __name__ == '__main__':
