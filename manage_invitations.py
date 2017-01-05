58e0960d921f622ea64af3310d6acb6dd1f8c9fd
Hal Wine <hwine@mozilla.com>
2017-01-04 16:08:21 -0800

    View & cancel invitations
    Uses preview API, so may not be stable.

---
+++ manage_invitations.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""
+    Check for pending org invitations
+
+"""
+import argparse
+import logging
+import arrow
+
+from client import get_github3_client
+# hack until invitations are supported upstream
+import github3
+if not hasattr(github3.orgs.Organization, 'invitations'):
+    raise NotImplementedError, ("Your version of github3.py does not support invitations. "
+                                "Try https://github.com/hwine/github3.py/tree/invitations")
+    
+
+logger = logging.getLogger(__name__)
+
+
+def get_cutoff_time(cutoff_delta):
+    k, v = cutoff_delta.split('=', 2)
+    args = {k: int(v)}
+    ok_after = arrow.now().replace(**args)
+    return ok_after
+
+
+def check_invites(gh, org_name, cancel=False, cutoff_delta="weeks=-2"):
+
+    org = gh.organization(org_name)
+    cutoff_time = get_cutoff_time(cutoff_delta)
+    for invite in org.invitations():
+        extended_at = arrow.get(invite['created_at'])
+        if extended_at < cutoff_time:
+            invite['ago'] = extended_at.humanize()
+            print('Cancel {login} ({email}) extended {ago}'.format(**invite))
+            if cancel:
+                org.remove_member(username=invite['login'])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--cancel', action='store_true',
+                        help='Cancel stale invitations')
+    parser.add_argument('--cutoff', help='When invitations go stale '
+                        '(default "weeks=-2")',
+                        default="weeks=-2")
+    parser.add_argument("orgs", nargs='*', default=['mozilla', ],
+                        help='github organizations to check (defaults to mozilla)')
+    # make sure arrow is happy with the cutoff syntax
+    args = parser.parse_args()
+    try:
+        get_cutoff_time(args.cutoff)
+    except (AttributeError, TypeError):
+        parser.error("invalid cutoff value")
+    return args
+
+
+def main():
+    args = parse_args()
+    if args.orgs:
+        gh = get_github3_client()
+        for org in args.orgs:
+            check_invites(gh, org, args.cancel, args.cutoff)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARN, format='%(asctime)s %(message)s')
+    main()
