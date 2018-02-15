8c7ad89e4c2a4902fb4890543cff41858e3f8bdd
Greg Guthe <g-k@users.noreply.github.com>
2018-02-15 18:54:28 -0500

    Add a multiorg code_search tool (#24)

--- client.py
+++ client.py
@@ -1,3 +1,7 @@
+
+from datetime import datetime
+import time
+
 from github3 import login
 
 
@@ -16,3 +20,20 @@ def get_github3_client():
     token = get_token()
     gh = login(token=token)
     return gh
+
+
+def sleep_if_rate_limited(gh, verbose=False):
+    rates = gh.rate_limit()
+
+    if not rates['resources']['search']['remaining']:
+        reset_epoch = rates['resources']['search']['reset']
+
+        reset_dt, now = datetime.utcfromtimestamp(reset_epoch), datetime.utcnow()
+
+        if reset_dt > now:
+            sleep_secs = (reset_dt - now).seconds + 1
+
+            if verbose:
+                print('sleeping for', sleep_secs, 'got rate limit', rates['resources']['search'])
+
+            time.sleep(sleep_secs)
