3c1d2715500a0d22457ce9c68acf544a34c533e4
Hal Wine <hwine@mozilla.com>
2019-12-10 17:08:01 -0800

    Manage repository collaborator invites
    Fixes GH-57
    
    Invitations can also be extended to outside collaborators at the
    repository level. These changes extend the existing code to poll for all
    such invitations (each repository in the organization must be
    interrogated).

--- manage_invitations.py
+++ manage_invitations.py
@@ -14,6 +14,12 @@ those with:
     manage_invitations | cut -d ' ' -f1
 Or invitee & inviter:
     manage_invitations | awk '!/^Proc/ {print $1 " by " $NF;}'
+
+ToDo:
+- Better handling of Security Advisory private repos. These return a 404
+  on invitation calls, which are simply reported now. These repos
+  usually have a name ending in the string '-ghsa-' followed by hex
+  digits.
 """
 # hwine believes keeping the doc above together is more important than PEP-8
 import argparse  # NOQA
@@ -28,6 +34,9 @@ if not hasattr(github3.orgs.Organization, 'invitations'):
     raise NotImplementedError("Your version of github3.py does not support "
                               "invitations. Try "
                               "https://github.com/hwine/github3.py/tree/invitations")  # NOQA
+if (1,3,0) > github3.__version_info__:
+    raise NotImplementedError("Your version of github3.py does not support "
+                              "collaborator invitations. Version '1.3.0' or later is known to work.")
 
 
 logger = logging.getLogger(__name__)
@@ -49,28 +58,70 @@ def check_invites(gh, org_name, cancel=False, cutoff_delta="weeks=-2"):
     cutoff_time = get_cutoff_time(cutoff_delta)
     try:
         for invite in org.invitations():
-            extended_at = arrow.get(invite['created_at'])
+            extended_at = arrow.get(invite.created_at)
             line_end = ": " if cancel else "\n"
             if extended_at < cutoff_time:
-                invite['ago'] = extended_at.humanize()
+                context = invite.as_dict()
+                context['ago'] = extended_at.humanize()
                 print('{login} ({email}) was invited {ago} by '
-                      '{inviter[login]}'.format(**invite),
+                      '{inviter[login]}'.format(**context),
                       end=line_end)
                 if cancel:
-                    success = org.remove_membership(username=invite['login'])
+                    success = org.remove_membership(invite.id)
                     if success:
                         print("Cancelled")
                     else:
                         print("FAILED to cancel")
                         logger.warning("Couldn't cancel invite for {login} "
-                                       "from {created_at}".format(**invite))
+                                       "from {created_at}".format(**context))
     except ForbiddenError:
         logger.error("You don't have 'admin:org' permissions for org '%s'",
                      org_name)
+    else:
+        # now handle collaborator invitations (GH-57)
+        for repo in org.repositories():
+            # occasionally get a 404 when looking for invitations.
+            # Assume this is a race condition and ignore. That may leave
+            # some invites uncanceled, but a 2nd run should catch.
+            try:
+                for invite in repo.invitations():
+                    extended_at = arrow.get(invite.created_at)
+                    line_end = ": " if cancel else "\n"
+                    if extended_at < cutoff_time:
+                        context = invite.as_dict()
+                        context['ago'] = extended_at.humanize()
+                        context['repo'] = repo.name
+                        context['inviter'] = invite.inviter.login
+                        context['invitee'] = invite.invitee.login
+                        print('{invitee} was invited to {repo} {ago} by '
+                            '{inviter} for {permissions} access.'.format(**context),
+                            end=line_end)
+                        if cancel:
+                            # Deletion not directly supported, so hack url &
+                            # use send delete verb directly
+                            delete_url = repo.url + "/invitations/" + str(invite.id)
+                            success = repo._delete(delete_url)
+                            if success:
+                                print("Cancelled")
+                            else:
+                                print("FAILED to cancel")
+                                logger.warning("Couldn't cancel invite for {login} "
+                                            "from {created_at}".format(**context))
+            except (github3.exceptions.NotFoundError,
+                    github3.exceptions.ConnectionError) as e:
+                # just report
+                logger.warning("Got 404 for invitation in {}, may be unhandled inviations. '{}'".format(repo.name,
+                            str(e)))
+
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description=__doc__, epilog=_epilog)
+    # from
+    # https://stackoverflow.com/questions/18462610/argumentparser-epilog-and-description-formatting-in-conjunction-with-argumentdef
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+        pass
+    parser = argparse.ArgumentParser(description=__doc__,
+            epilog=_epilog, formatter_class=CustomFormatter)
     parser.add_argument('--cancel', action='store_true',
                         help='Cancel stale invitations')
     parser.add_argument('--cutoff', help='When invitations go stale '
@@ -100,4 +151,7 @@ def main():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARN, format='%(asctime)s %(message)s')
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        raise SystemExit("\nCancelled by user")
